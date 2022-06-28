64f981b271dec716cb90a3e18356f56f70e2da7a
pishifat <pishifat@gmail.com>
2022-06-28 10:57:38 -0700

    news generation

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 generated this https://osu.ppy.sh/wiki/en/Contests/A_Labour_of_Love_Contest#teams (excluding the link column)
 
-uploading for potential future use
+now it does more (but not in a convenient way):
+
+`alol.js` -> a labour of love contest. reads from `teams.csv` using its team submission form format
+`triangles.js` -> triangles contest. reads from `triangles.csv` using its team submission form format
+`news.js` -> user flags and profile links for news posts. reads from `news.csv` separating user IDs by newlines
---
+++ news.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const secret = require("./secret.json");
+const axios = require("axios");
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function generate() {
+    const buffer = fs.readFileSync('news.csv');
+    const csv = buffer.toString();
+
+    if (!csv) {
+        console.log(`couldn't read csv`);
+    }
+
+    const data = csv.split('\r\n');
+
+    let table = '';
+
+    for (const id of data) {
+        await sleep(1000);
+        const user = await getUser(id);
+        table += `![${user.country} flag](/wiki/shared/flag/${user.country}.gif) [${user.username}](https://osu.ppy.sh/users/${user.user_id})`;
+        table += '\n';
+    }
+
+    fs.writeFile('table.txt', table, (error) => {
+        if (error) throw err;
+    });
+
+    console.log('done');
+}
+
+async function getUser(id) {
+    const url = `https://osu.ppy.sh/api/get_user?k=${secret.token}&u=${id}`;
+    const res = await axios.get(url);
+
+    console.log(res.data[0].username);
+
+    return res.data[0];
+}
+
+generate();
